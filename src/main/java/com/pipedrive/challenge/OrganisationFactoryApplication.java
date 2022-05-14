79e8653764a7179d920a5262aa119411583b775a
rps04 <rui.peres@sky.uk>
2022-05-14 18:52:02 +0100

    project setup

---
+++ src/main/java/com/pipedrive/challenge/OrganisationFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.pipedrive.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganisationFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganisationFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pipedrive/challenge/OrganisationFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipedrive.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganisationFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
